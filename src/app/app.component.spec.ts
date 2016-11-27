ee11ea5516fc29bfc192acbfc8ea7a9a608a307b
vampot <vamshi.pothuka@syncron.com>
2016-11-27 14:31:23 -0800

    first commit having the error message

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -2,32 +2,28 @@
 
 import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import {MaterialModule} from '@angular/material';
+import {FormsModule} from '@angular/forms';
 
 describe('AppComponent', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
+      imports: [
+        FormsModule,
+        MaterialModule.forRoot()
+      ],
       declarations: [
-        AppComponent
+        AppComponent,
       ],
     });
   });
 
-  it('should create the app', async(() => {
-    let fixture = TestBed.createComponent(AppComponent);
-    let app = fixture.debugElement.componentInstance;
-    expect(app).toBeTruthy();
-  }));
 
-  it(`should have as title 'app works!'`, async(() => {
-    let fixture = TestBed.createComponent(AppComponent);
-    let app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('app works!');
-  }));
-
-  it('should render title in a h1 tag', async(() => {
+  it('should be checked', async(()=>{
     let fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     let compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('app works!');
+    expect(compiled.querySelectorAll('md-checkbox').length).toBe(1);
+    expect(compiled.querySelector('md-checkbox').classList).toContain('md-checkbox-checked');
   }));
 });
